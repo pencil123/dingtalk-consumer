6870a3e3e33a2b7f3473f15acc4d5f66e5a3f0bb
lyzhang <tianxiamall@163.com>
2020-11-04 16:11:35 +0800

    添加mq

---
+++ src/main/java/cn/blogs/dingtalkconsumer/rabbitmq/DirectReceiver.java
@@ -0,0 +1,22 @@
+package cn.blogs.dingtalkconsumer.rabbitmq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+public class DirectReceiver {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+}
