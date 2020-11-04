640bb39e6ac07d9ccc8fb424fef3c4803fe74a4e
lyzhang <tianxiamall@163.com>
2020-11-04 15:52:44 +0800

    初始化

---
+++ src/test/java/cn/blogs/dingtalkconsumer/DingtalkConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.blogs.dingtalkconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingtalkConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
